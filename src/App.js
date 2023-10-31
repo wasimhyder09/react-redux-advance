c48c100385aab394951e531c62977061b2e80d33
Wasim Hyder <wasim@creativefaze.com>
2023-10-31 22:53:14 +0500

    Handled side effect in component with redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { UseSelector, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://redux-cart-c3144-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
